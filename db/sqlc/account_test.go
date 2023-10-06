2c3786be3f78ce6612032fb65c8d63634800a8c8
tasyaprajna <prajna.taasya@binus.ac.id>
2023-10-06 11:27:17 +0700

    fixing make issue

--- db/sqlc/account_test.go
+++ db/sqlc/account_test.go
@@ -7,7 +7,7 @@ import (
 	"time"
 
 	"github.com/stretchr/testify/require"
-	"github.com/tasyaprajna/simplebank/db/util"
+	"github.com/tasyaprajna/simplebank/util"
 )
 
 func createRandomAccount(t *testing.T) Accounts {
